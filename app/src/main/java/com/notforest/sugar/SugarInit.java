e215ab1505671737e675fea86a46782a75326c60
Notforest <notforest69@gmail.com>
2024-03-25 17:12:57 +0100

    Added custom Java class for app initialization on both front-end and back-end side.

---
+++ app/src/main/java/com/notforest/sugar/SugarInit.java
@@ -0,0 +1,16 @@
+/*
+ *  Custom helper class for initialization the environment.
+ * */
+
+package com.notforest.sugar;
+
+public class SugarInit {
+    // Loads external dynamic libraries.
+    static {
+        System.loadLibrary("sugar_jni");
+    }
+
+    // Native JNI interface for Rust backend.
+    /* Initialized all important tasks on Rust's backend side. */
+    public static native void rustInit();
+}

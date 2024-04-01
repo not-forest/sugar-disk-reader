8172e69352ce2b931562b34717d0c0be58359419
Notforest <notforest69@gmail.com>
2024-04-01 21:31:54 +0200

    Now signup activity can parse most of the errors that might occur.

--- app/src/main/java/com/notforest/sugar/SignupActivity.java
+++ app/src/main/java/com/notforest/sugar/SignupActivity.java
@@ -6,11 +6,14 @@
 
 package com.notforest.sugar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,7 +31,7 @@ public class SignupActivity extends AppCompatActivity {
     // Text input values.
     EditText editTextEmail, editTextPassword, editTextConfirmPassword;
     // Link to login activity.
-    TextView loginRedirect;
+    TextView loginRedirect, mailErrorText, passwordErrorText, confirmPasswordErrorText;
     Button buttonSignUp;
 
     @Override
@@ -43,20 +46,107 @@ public class SignupActivity extends AppCompatActivity {
         loginRedirect =             findViewById(R.id.loginRedirect);
         buttonSignUp =              findViewById(R.id.buttonSignUp);
 
+        mailErrorText =             findViewById(R.id.mailErrorText);
+        passwordErrorText =         findViewById(R.id.passwordErrorText);
+        confirmPasswordErrorText =  findViewById(R.id.confirmPasswordErrorText);
+
         // Setting up listeners.
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mailErrorText.setVisibility(View.INVISIBLE);
+                passwordErrorText.setVisibility(View.INVISIBLE);
+                confirmPasswordErrorText.setVisibility(View.INVISIBLE);
+
                 // Reading from fields.
                 String mail = editTextEmail.getText().toString();
                 String pass = editTextPassword.getText().toString();
                 String conf = editTextConfirmPassword.getText().toString();
 
                 if (pass.equals(conf)) {
-                    // Giving control to backend.
-                    signUp(mail, pass, conf);
+                    if (ifNotEmpty(mail, pass, conf)) {
+                        // Giving control to backend.
+                        int output = signUp(mail, pass, conf);
+                        // Rust's backend function will return one of many defined status codes.
+                        switch (output) {
+                            case 0:
+                                // Note about signup success.
+                                Toast.makeText(
+                                        SignupActivity.this,
+                                        "Created account successfully! Please login.",
+                                        Toast.LENGTH_SHORT
+                                ).show();
+
+                                // Jumping to login activity
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                break;
+                            case 10:
+                                mailErrorText.setText(R.string.error_mail_regex);
+                                mailErrorText.setVisibility(View.VISIBLE);
+                                break;
+                            case 11:
+                                passwordErrorText.setText(R.string.error_pass_weak);
+                                passwordErrorText.setVisibility(View.VISIBLE);
+                                break;
+                            case 20:
+                                mailErrorText.setText(R.string.error_mail_used);
+                                mailErrorText.setVisibility(View.VISIBLE);
+                                break;
+                            case 21:
+                                Toast.makeText(
+                                        SignupActivity.this,
+                                        "Your 'Sugar' account was disabled due to strange activity from this device. Please contact administration for further support.",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                                break;
+                            default:
+                                // Internal error.
+                                Toast.makeText(
+                                        SignupActivity.this,
+                                        "Internal error has occur",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                        }
+                    }
+                } else {
+                    confirmPasswordErrorText.setText(R.string.error_pass_nomatch);
+                    confirmPasswordErrorText.setVisibility(View.VISIBLE);
                 }
             }
         });
+    }
+
+    /* Checks if all user inputs and returns true if they are. */
+    private boolean ifNotEmpty(String m, String p, String f) {
+        boolean not_empty = true;
+
+        // Mail field.
+        if (m.isEmpty()) {
+            mailErrorText.setText(R.string.error_mail_empty);
+            mailErrorText.setVisibility(View.VISIBLE);
+            not_empty = false;
+        } else {
+            mailErrorText.setVisibility(View.INVISIBLE);
+        }
+
+        // Password field.
+        if (p.isEmpty()) {
+            passwordErrorText.setText(R.string.error_pass_empty);
+            passwordErrorText.setVisibility(View.VISIBLE);
+            not_empty = false;
+        } else {
+            passwordErrorText.setVisibility(View.INVISIBLE);
+        }
+
+        // Password field.
+        if (f.isEmpty()) {
+            confirmPasswordErrorText.setText(R.string.error_confirm_password_empty);
+            confirmPasswordErrorText.setVisibility(View.VISIBLE);
+            not_empty = false;
+        } else {
+            confirmPasswordErrorText.setVisibility(View.INVISIBLE);
+        }
+
+        return not_empty;
     }
 }

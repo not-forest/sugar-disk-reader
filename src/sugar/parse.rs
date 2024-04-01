43801fa9cf92261b76920b952343e972c14bd515
Notforest <notforest69@gmail.com>
2024-04-01 21:37:19 +0200

    The parse struct is not needed for now as the firebase does the parsing itself. Hovewer it will be probably used later for the main activity.

--- src/sugar/parse.rs
+++ src/sugar/parse.rs
@@ -3,7 +3,6 @@
 //! This module parses user's input from login, singup, configurations and data from
 //! target computer.
 
-use super::auth::errors::DataParseError;
 use regex::Regex;
 use once_cell::sync::Lazy;
 
@@ -14,37 +13,5 @@ use once_cell::sync::Lazy;
 pub struct SugarParser;
 
 impl SugarParser {
-    /// Parses provided user's mail.
-    pub(crate) fn parse_mail(mail: String) -> Result<(), DataParseError> {
-        // This static regex is checking email for correctness.
-        static REG: Lazy<Regex> = Lazy::new(|| 
-            Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap()
-        );
 
-        if !REG.is_match(&mail) {
-            return Err(DataParseError::MailRegexError)
-        }
-
-        log::debug!("OK");
-        Ok(())
-    }
-
-    /// Parses provided user's password.
-    pub(crate) fn parse_pass(pass: String) -> Result<(), DataParseError> {
-        // This static regex is checking if password conditions are met.
-        //
-        // # Conditions:
-        // - At least 4 characters long.
-        // - At least one special character.
-        static REG: Lazy<Regex> = Lazy::new(|| 
-            Regex::new(r"^(?=.*[!@#$%^&*()_+{}|:<>`\-=[\];,./])(.{4,})$").unwrap()
-        );
-
-        if !REG.is_match(&pass) {
-            return Err(DataParseError::PasswordConditionsError)
-        }
-
-        log::debug!("OK");
-        Ok(())
-    }
 }

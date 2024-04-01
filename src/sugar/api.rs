2a9b378249332e74e944c04a12421a1780a9b992
Notforest <notforest69@gmail.com>
2024-04-01 21:34:00 +0200

    New API related links.

--- src/sugar/api.rs
+++ src/sugar/api.rs
@@ -1,5 +1,6 @@
 //! This module defines all symbols related to API urls and keys.
 
-/// Constant string literal for firebase URI
-pub const FIREBASE_URI: &'static str = "";
-
+/// Constant string literal for firebase's database URI
+pub static FIREBASE_URI: &'static str = "https://sugar-dff57-default-rtdb.europe-west1.firebasedatabase.app";
+/// Application's API KEY.
+pub static FIREBASE_API_KEY: &'static str = "AIzaSyCV-DaeUbuIs6Pribh8BghivWl6uowgLlQ";

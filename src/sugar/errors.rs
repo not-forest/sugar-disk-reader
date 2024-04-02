bbc31a8ccecffe3f653760d28c41f4f190a42f66
Notforest <notforest69@gmail.com>
2024-04-02 22:28:45 +0200

    Added all related Display implementations for all errors. Added new StorageError for parsing errors in IO operations with storages.

--- src/sugar/errors.rs
+++ src/sugar/errors.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 
 use firebase_auth_sdk::Error;
+use std::fmt::Display;
 
 /// Internal application errors.
 ///
@@ -13,6 +14,29 @@ pub enum InternalError {
     TOKIO_THREAD_ERROR = 1,
     /// Will occur if unable to connect to some service. This is most likely a networking issue.
     NETWORK_ERROR = 2,
+}
+
+/// Application error related to storage manipulations.
+///
+/// Basically flags if the read or write operation was done successfully or not. It is used with
+/// both local storage and cloud alternatives.
+#[repr(u8)]
+pub enum StorageError {
+    /// if trying to read from non-existing file.
+    FILE_NOT_EXIST = 40,
+    /// Either read or write is taking too much time and caused an IO timeout. 
+    TIME_OUT = 41,
+    /// The data's content does not match it's destination.
+    BAD_DATA = 42,
+    /// No data is provided for writing, or the file is empty.
+    NO_DATA = 43,
+    /// This can only occur if serde's Serialize/Deserialize macro will somehow fail. Not very likely. 
+    SERIALIZATION_ERROR = 44,
+    /// Not enough memory to perform read or write. This can be handled from the software side and
+    /// fixed right away.
+    OUT_OF_MEMORY = 45,
+    /// An IO operation was interrupted.
+    INTERRUPTED = 46,
 }
 
 /// Errors which occur during signup.
@@ -40,6 +64,51 @@ pub enum LoginError {
     INVALID_LOGIN_CREDENTIALS = 30,
     /// User was disabled by an administrator.
     USER_DISABLED = 31,
+}
+
+impl Display for InternalError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::TOKIO_THREAD_ERROR => write!(f, "Tokio Thread did not exit successfully"),
+            Self::NETWORK_ERROR => write!(f, "Network error. No internet connection."),
+        }
+    }
+}
+
+impl Display for StorageError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FILE_NOT_EXIST => write!(f, "IO operation failed: File does not exist."),
+            Self::BAD_DATA => write!(f, "IO operation failed: data's content does not match it's destination."),
+            Self::NO_DATA => write!(f, "IO operation failed: No data."),
+            Self::SERIALIZATION_ERROR => write!(f, "IO operation failed: Serde serialization/deserialization failed."),
+            Self::TIME_OUT => write!(f, "IO operation failed: Operation timed out."),
+            Self::INTERRUPTED => write!(f, "IO operation failed: Interrupted, retrying..."),
+            Self::OUT_OF_MEMORY => write!(f, "IO opertation failed: Out of memory, retrying..."),
+        }
+    }
+}
+
+impl Display for SignupError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::INVALID_EMAIL => write!(f, "Invalid email regex was provided."),
+            Self::INVALID_PASS => write!(f, "Provided password is too weak."),
+            Self::EMAIL_EXISTS => write!(f, "Email exists, login required."),
+            Self::TOO_MANY_ATTEMPTS => write!(f, "Too many attepts. Waiting on CD."),
+        }
+    }
+}
+
+impl Display for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::INVALID_EMAIL => write!(f, "Invalid email regex was provided."),
+            Self::INVALID_PASS => write!(f, "Provided password is too weak."),
+            Self::INVALID_LOGIN_CREDENTIALS => write!(f, "Either password or email is wrong. Retry is needed."),
+            Self::USER_DISABLED => write!(f, "User is disabled by application administrator."),
+        }
+    }
 }
 
 impl Into<SignupError> for Error {

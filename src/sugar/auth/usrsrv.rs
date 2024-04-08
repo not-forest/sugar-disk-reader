e812b69fa8a914ce8f4b054041076e76c788a3c1
Notforest <notforest69@gmail.com>
2024-04-08 19:02:49 +0200

    New fast login method for logging in with firebase token. If it is expired or any other internal/external error will occur, a regular login is required.

--- src/sugar/auth/usrsrv.rs
+++ src/sugar/auth/usrsrv.rs
@@ -3,7 +3,7 @@
 //! This module handles all events related to user authentications, which includes logins,
 //! registration and modifications requested by users.
 
-use crate::sugar::errors::{InternalError, SignupError, LoginError};
+use crate::sugar::errors::{InternalError, LoginError, SignupError, StorageError};
 
 /// Special wrapper around service status number, that will be returned to the front-end.
 #[repr(u8)]
@@ -12,6 +12,8 @@ pub enum UserServiceStatus {
     NoError = 0,
     /// Error, which comes from the application itself.
     InternalError(InternalError),
+    /// Error was related to storage operation.
+    StorageError(StorageError),
     /// Errors from the firebase server.
     SignupError(SignupError),
     LoginError(LoginError),
@@ -23,13 +25,85 @@ pub mod service {
 
     use super::UserServiceStatus;
     use crate::sugar::{
-        api::FIREBASE_API_KEY, errors::StorageError, storage::LocalStorage
+        api::FIREBASE_API_KEY, errors::{StorageError, LoginError}, storage::LocalStorage
     };
+
+    /// Performs fast login via firebase token.
+    ///
+    /// This will omit the need of writing login credentials each time. Only works for logged in
+    /// users. If the token is expired, user must login once more.
+    #[tokio::main]
+    pub async fn fast_login() -> UserServiceStatus {
+        log::debug!("Encountered fast login request.");
+        use firebase_auth_sdk::api::SignInResponse;
+        use firebase_auth_sdk::Error;
+
+        match LocalStorage::read::<SignInResponse>("login_response") {
+            Ok(mut res) => {
+                let mut token = res.id_token;
+                let auth = FireAuth::new(FIREBASE_API_KEY.to_string());
+               
+                'token: loop {
+                    // Verifying the current ID token.
+                    return match auth.verify_id_token(token.as_str()).await {
+                        Ok(claim) => {
+                            log::info!("Token login success: {:#?}", claim);
+
+                            UserServiceStatus::NoError
+                        },
+                        Err(err) => match err.clone() {
+                            Error::API(s) => {
+                                log::error!("Firebase API error: {}", s);
+                                UserServiceStatus::LoginError(
+                                    LoginError::KEY_PARSING_ERROR
+                                )
+                            }, 
+                            Error::Token(s) => {
+                                log::error!("Token error: {}", s);
+
+                                let token_err = err.into();
+
+                                // Trying to refresh the expired token right away.
+                                match token_err {
+                                    // If expired, refreshing.
+                                    LoginError::TOKEN_EXPIRED => {
+                                        if let Some(refresh_token) = res.refresh_token {
+                                            match auth.refresh_id_token(refresh_token.as_str()).await {
+                                                Ok(claim) => {
+                                                    log::info!("Token refreshed, retrying to verify. Claim: {:#?}", claim);
+                                                    token = claim.id_token;
+                                                    res.id_token = token.clone();
+                                                    res.refresh_token = Some(claim.refresh_token);
+
+                                                    // Writing new token data to the
+                                                    LocalStorage::write(&res, "login_response").ok();
+                                                },
+                                                Err(err) => {
+                                                    log::error!("Unable to refresh the token. Login is required: {:#?}", err);
+                                                    return UserServiceStatus::LoginError(token_err)
+                                                },
+                                            };
+                                        }
+
+                                        // Retrying with new token.
+                                        continue 'token
+                                    },
+                                    _ => UserServiceStatus::LoginError(token_err),
+                                }
+                            },
+                            _ => unreachable!(),
+                        },
+                    }
+                }
+            },
+            Err(err) => UserServiceStatus::StorageError(err),
+        }
+    }
 
     /// Performs full application login procedure.
     ///
     /// Performs communication with firebase server and provides full login routine. Starts user's
-    /// session if data match and 
+    /// session if data will match.
     #[tokio::main]
     pub async fn login(mail: String, pass: String) -> UserServiceStatus { 
         log::debug!("Encountered login request with data: {:#?}", (&mail, &pass));
@@ -130,6 +204,7 @@ impl Into<u8> for UserServiceStatus {
             Self::InternalError(err) => err as u8,
             Self::SignupError(err) => err as u8,
             Self::LoginError(err) => err as u8,
+            Self::StorageError(err) => err as u8,
         }
     }
 }

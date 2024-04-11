829d99d13f7cd9e10b456e3de9de76c6940753c6
Notforest <notforest69@gmail.com>
2024-04-11 21:32:23 +0200

    Added the first steps forward daemon. Currently initializes IDT with handler functions.

--- daemon/src/main.c
+++ daemon/src/main.c
@@ -13,9 +13,65 @@
  *  to hijack any data it the disk space.
  * */
 
+extern void *IDT_TABLE[];       // IDT TABLE defined in idt.asm 
+
 #include <stdint.h>
+#include "arch/idt.h"
+#include "vga.h"
+
+#define L_WARN COLOR_YELLOW
+#define L_OK   COLOR_GREEN
+#define L_INFO COLOR_WHITE
+
+#define OK "[OK]"
+#define FD "[FAILED]"
+
+// A global static buffer.
+static volatile VGABuffer LOGGER = {.row = 0, .col = 0};
 
 /* Main daemon entry point. */
 void main(uint16_t boot_drive) {
+    disable_cursor(); 
+
+#if __RELEASE__ == 0
+    prints("Initializing... ", L_INFO, &LOGGER); 
+#endif
+
+    /* Beginning of daemon post initialization */
+    
+    idt_init();
+
+    /* End of initialization */
+    
+#if __RELEASE__ == 0
+    println(OK, L_OK, &LOGGER);
+#endif
+
     for(;;);
+}
+
+/* Initializing the IDT and putting required handler functions within */
+void idt_init() {
+    uint16_t cs;
+    uint8_t vec;
+
+    // Reading the cs segment.
+    __asm__("mov %%cs, %w0\n":"=a"(cs)::"memory");
+
+    // Providing exceptions,
+    for (vec = 0; vec < 32; vec++) {
+        idt_set_descriptor(vec, IDT_TABLE[vec], TRAP_GATE, cs);
+    }
+
+    for (; vec < 255; vec++) {
+        idt_set_descriptor(vec, IDT_TABLE[vec], INT_GATE, cs);
+    }
+
+    // Creating a pointer for lidt instruction
+    struct {
+        uint16_t length;
+        void*    base;
+    } __attribute__((packed)) IDTR = { .length = sizeof(IDT) - 1, &IDT };
+ 
+    __asm__ ( "lidt %0" : : "m"(IDTR) );
 }

1ac691331c08de12e252ad7211933d028cd72622
Notforest <notforest69@gmail.com>
2024-04-11 21:35:44 +0200

    Added header that defines CPU related data types and handler functions for exceptions and interrupts.

---
+++ daemon/src/arch/cpu.h
@@ -0,0 +1,71 @@
+/*
+ *  Header file that defines CPU related features and structures.
+ * */
+
+#ifndef __CPU__h
+#define __CPU__h
+
+#include<stdint.h>
+
+/* 
+ *  A special struct that is being pushed to the stack by CPU when some exception or interrupt occurs.
+ *
+ *  Daemon is running in 32-bit protected mode to support or types of devices and to not cause overhead
+ *  of long mode. Because of that only three registers will be pushed onto the stack during an interrupt.
+ * */
+struct Iframe {
+    uint16_t eip, cs, eflags;
+}; 
+
+//////// CPU DEFINED INTERRUPTS AND EXCEPTIONS /////////
+
+#define general_handler(name) \
+    __attribute__((interrupt, weak)) \
+    void name(struct Iframe *frame);
+
+/* * * * * * * * * * * * * * * * * * * *
+ *  EXCEPTION HANDLERS (vecn: 0 - 31)
+ *
+ *  Defined by CPU as exceptions and shall be provided with handler function.
+ * * * * * * * * * * * * * * * * * * * */
+
+/*
+ *  GENERAL HANDLER
+ *
+ *  Will be aplied to all undefined handler functions to prevent double fault. 
+ * */
+general_handler(GENERAL_HANDLER)
+general_handler(DIVISION_ERROR_HANDLER);
+general_handler(DEBUG_HANDLER);
+general_handler(NMI_HANDLER);
+general_handler(BREAKPOINT_HANDLER);
+general_handler(OVERFLOW_HANDLER);
+general_handler(BRE_HANDLER);
+general_handler(INVALID_OPCODE_HANDLER);
+general_handler(DEVICE_NOT_AVAILABLE_HANDLER);
+general_handler(DOUBLE_FAULT_HANDLER);
+general_handler(INVALID_TSS_HANDLER);
+general_handler(SEGMENT_NOT_PRESENT_HANDLER);
+general_handler(STACK_SEGMENT_FAULT_HANDLER);
+general_handler(GENERAL_PROTECTION_FAULT_HANDLER);
+general_handler(X87_FP_EXCEPTION_HANDLER);
+general_handler(ALIGNMENT_CHECK_HANDLER);
+general_handler(MACHINE_CHECK_HANDLER);
+general_handler(SIMD_FP_EXCEPTION_HANDLER);
+general_handler(VIRTUALIZATION_EXCEPTION_HANDLER);
+general_handler(CONTROL_PROTECTION_HANDLER);
+general_handler(HIE_HANDLER);
+general_handler(VMMC_EXCEPTION_HANDLER);
+general_handler(SECURITY_EXCEPTION_HANDLER);
+
+
+/* * * * * * * * * * * * * * * * * * * *
+ *  SOFTWARE INTERRUPTS (vecn: 32 - 255)
+ *
+ *  Defined by this daemon and can only be cause via certain pheripherals like PIC or
+ *  via software part itself.
+ * * * * * * * * * * * * * * * * * * * */
+general_handler(SOFTWARE_TIMER_HANDLER);
+general_handler(SOFTWARE_KEYBOARD_HANDLER);
+
+#endif

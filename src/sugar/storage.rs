48accd37975a65e8b96cf0ca79a9d1ad3b11c790
Notforest <notforest69@gmail.com>
2024-04-14 16:50:52 +0200

    Fixed storage errors, which prevented fast login from working properly.

--- src/sugar/storage.rs
+++ src/sugar/storage.rs
@@ -5,7 +5,7 @@
 //! However it also provide an interface to Java's front-end for communicating with cloud storage.
 
 use lazy_static::lazy_static;
-use std::{fs::File, io::ErrorKind, path::{Path, PathBuf}, sync::RwLock};
+use std::{fs::File, mem::size_of, io::ErrorKind, path::{Path, PathBuf}, sync::RwLock};
 use serde::{de::DeserializeOwned, Serialize};
 use super::errors::StorageError; 
 
@@ -29,35 +29,33 @@ impl LocalStorage {
     ///
     /// If write fails, will return one of pre defined errors that match it's result. If everything
     /// will go accordingly, will return the amount of bytes written to file.
-    pub fn write(data: &impl Serialize, dest: &'static str) -> Result<usize, StorageError> {
+    pub fn write<T>(data: &T, dest: &'static str) -> Result<usize, StorageError> where
+        T: Serialize
+    {
         let dest = Path::new(
             FILES_DIR.read().unwrap().as_ref()
         ).join(dest.to_owned()).with_extension("json");
 
         let out = {
-            if let Ok(data) = serde_json::to_string(data).as_ref() {
-                let length = data.len();
-                log::info!("Writing {} bytes to local storage: {}", length, dest.to_string_lossy());
+            let length = size_of::<T>();
+            log::info!("Writing {} bytes to local storage: {}", length, dest.to_string_lossy());
 
-                if length == 0 {
-                    return Err(StorageError::NO_DATA)
-                };
+            if length == 0 {
+                return Err(StorageError::NO_DATA)
+            };
 
-                match File::create(dest) {
-                    Ok(buf_writer) => match serde_json::to_writer(buf_writer, data) {
-                        Ok(_) => Ok(length),
-                        Err(_) => Err(StorageError::SERIALIZATION_ERROR),
-                    },
-                    Err(err) => match err.kind() {
-                        ErrorKind::TimedOut => Err(StorageError::TIME_OUT),
-                        ErrorKind::InvalidData => Err(StorageError::BAD_DATA),
-                        ErrorKind::Interrupted => Err(StorageError::INTERRUPTED),
-                        ErrorKind::OutOfMemory => Err(StorageError::OUT_OF_MEMORY),
-                        _ => unreachable!(),
-                    },
-                }
-            } else {
-                Err(StorageError::SERIALIZATION_ERROR)
+            match File::create(dest) {
+                Ok(buf_writer) => match serde_json::to_writer(buf_writer, data) {
+                    Ok(_) => Ok(length),
+                    Err(_) => Err(StorageError::SERIALIZATION_ERROR),
+                },
+                Err(err) => match err.kind() {
+                    ErrorKind::TimedOut => Err(StorageError::TIME_OUT),
+                    ErrorKind::InvalidData => Err(StorageError::BAD_DATA),
+                    ErrorKind::Interrupted => Err(StorageError::INTERRUPTED),
+                    ErrorKind::OutOfMemory => Err(StorageError::OUT_OF_MEMORY),
+                    _ => unreachable!(),
+                },
             }
         };
 
@@ -80,9 +78,14 @@ impl LocalStorage {
         let out = {
             log::info!("Reading data from local storage: {}", dest.to_string_lossy());
             match File::open(dest) {
-                Ok(buf_reader) => match serde_json::from_reader(buf_reader) {
-                    Ok(data) => Ok(data),
-                    Err(_) => Err(StorageError::SERIALIZATION_ERROR),
+                Ok(buf_reader) => {
+                    match serde_json::from_reader(buf_reader) {
+                        Ok(data) => Ok(data),
+                        Err(err) => {
+                            log::error!("Serde error: {}", err);
+                            Err(StorageError::SERIALIZATION_ERROR)
+                        },
+                    }
                 },
                 Err(err) => match err.kind() {
                     ErrorKind::NotFound => Err(StorageError::FILE_NOT_EXIST),
